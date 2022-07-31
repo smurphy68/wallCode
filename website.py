93f1514c87743db1aead109e74896861cf9fa50b
smurphy68 <smurphy68@live.co.uk>
2022-07-31 11:38:12 +0100

    change

--- website.py
+++ website.py
@@ -78,8 +78,31 @@ def theRecord():
 
 @app.route('/Set')
 def theForge():
-    return open("theForge.html")
-
+    return """
+    <head>
+        <title>Milestone Wall</title>
+    </head>
+    <body>
+        <h1>The Forge</h1>
+            <p><a href="./">Home<a></p>
+            <p><a href="./Routes">the Record<a></p>
+            <p><a href="./Set">the Forge<a></p><div></div>
+        <h2>Set a Route!</h2>
+            <form action="/SubmitRoute">
+                <label for="User">User:</label><br>
+                <input type="text" id="User" name="User" value="Name"><br>
+                <label for="Route Name">Name of Route:</label><br>
+                <input type="text" id="Name" name="Name" value="Route Name"><br>
+                <label for="Route Grade">Grade of Route:</label><br>
+                <input type="text" id="Grade" name="Grade" value="Route Grade"><br>
+                <label for="Number of Attempts">Number of Attempts</label><br>
+                <input type="text" id="Attempts" name="Attempts" value="Number of Attempts"><br><br>
+                <input type="submit" value="Submit">
+            </form>
+        <h2>Select Holds!</h2>
+            <img src="Board.png" width="500" height="600" />
+    </body>
+    """
 @app.route('/SubmitRoute', methods=["POST", "GET"])
 def addRoute(): 
     if request.method == "GET":
