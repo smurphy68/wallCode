f36413a425050247a49adf6e5ee3b417d48c1c9a
smurphy68 <smurphy68@live.co.uk>
2022-09-03 18:32:06 +0100

    changeState working

--- scripts/buttonfuncs.js
+++ scripts/buttonfuncs.js
@@ -1,38 +1,51 @@
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map( button=> {
-    button.addEventListener('click', (e) => {
-        let hold = Holds[e.target.innerHTML]
-        hold.changeState()
-        console.log(hold);
-    })
-})
-
 class Hold {
-    constructor(holdID, state = "") {
+    constructor(holdID, state="off") {
         this.holdID = holdID;
         this.state = state;
     }
     changeState() {
-        const states = ["start", "route" , "foot", "end", "off"];
-        if (this.state = "off") {
-            console.log("[OLD STATE]: " + this.state);
-            this.state = states[0];
-            console.log("[NEW STATE]: " + this.state);
-        } else {
-            //check old index state
-            console.log("[OLD STATE]: " + this.state);
-            //get index of current state in states
-            index = states.indexOf(this.state);
-            //new state == states[index+1]
-            this.state = states[index+1];
-            console.log("[NEW STATE]: " + this.state);
+        //const states = ["start", "route" , "foot", "end", "off"];
+        switch (this.state) {
+            case (this.state = "off"):
+                console.log("[OLD STATE]: " + this.state);
+                this.state = "start";
+                console.log("[NEW STATE]: " + this.state);
+                break
+            case (this.state = "start"):
+                console.log("[OLD STATE]: " + this.state);
+                this.state = "route";
+                console.log("[NEW STATE]: " + this.state);
+                break
+            case (this.state = "route"):
+                console.log("[OLD STATE]: " + this.state);
+                this.state = "foot";
+                console.log("[NEW STATE]: " + this.state);
+                break
+            case (this.state = "foot"):
+                console.log("[OLD STATE]: " + this.state);
+                this.state = "end";
+                console.log("[NEW STATE]: " + this.state);
+                break
+            case (this.state = "end"):
+                console.log("[OLD STATE]: " + this.state);
+                this.state = "off";
+                console.log("[NEW STATE]: " + this.state);
+                break
         }
     }  
 }
 
-var Holds = []
+let Holds = []
 
 for (let i = 0; i < buttons.length; i++) {
     Holds[buttons[i].innerHTML] = new Hold(buttons[i].innerHTML);
 }
+
+buttons.map( button=> {
+    button.addEventListener('click', (e) => {
+        let hold = Holds[e.target.innerHTML]
+        hold.changeState()
+    })
+})
