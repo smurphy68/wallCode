e47a2e5b4669fdda397d558d4d595dc31ebb4954
smurphy68 <smurphy68@live.co.uk>
2022-09-03 19:04:00 +0100

    SM Testing

--- scripts/buttonfuncs.js
+++ scripts/buttonfuncs.js
@@ -1,37 +1,42 @@
 let buttons = Array.from(document.getElementsByClassName('button'));
 
 class Hold {
-    constructor(holdID, state="off") {
+    constructor(holdID, colour="", state="off") {
         this.holdID = holdID;
         this.state = state;
+        this.colour = colour
     }
     changeState() {
-        //const states = ["start", "route" , "foot", "end", "off"];
         switch (this.state) {
             case (this.state = "off"):
-                console.log("[OLD STATE]: " + this.state);
+                //console.log(`[OLD STATE]: ${this.holdID} is ${this.state}.`);
                 this.state = "start";
-                console.log("[NEW STATE]: " + this.state);
+                this.colour = "green"
+                console.log(`[NEW STATE]: ${this.holdID} is a ${this.state} hold.`);
                 break
             case (this.state = "start"):
-                console.log("[OLD STATE]: " + this.state);
+                //console.log(`[OLD STATE]: ${this.holdID} is a ${this.state} hold.`);
                 this.state = "route";
-                console.log("[NEW STATE]: " + this.state);
+                this.colour = "blue"
+                console.log(`[NEW STATE]: ${this.holdID} is a ${this.state} hold.`);
                 break
             case (this.state = "route"):
-                console.log("[OLD STATE]: " + this.state);
+                //console.log(`[OLD STATE]: ${this.holdID} is a ${this.state} hold.`);
                 this.state = "foot";
-                console.log("[NEW STATE]: " + this.state);
+                this.colour = "aqua"
+                console.log(`[NEW STATE]: ${this.holdID} is a ${this.state} hold.`);
                 break
             case (this.state = "foot"):
-                console.log("[OLD STATE]: " + this.state);
+                //console.log(`[OLD STATE]: ${this.holdID} is a ${this.state} hold.`);
                 this.state = "end";
-                console.log("[NEW STATE]: " + this.state);
+                this.colour = "red"
+                console.log(`[NEW STATE]: ${this.holdID} is a ${this.state} hold.`);
                 break
             case (this.state = "end"):
-                console.log("[OLD STATE]: " + this.state);
+                //console.log(`[OLD STATE]: ${this.holdID} is a ${this.state} hold.`);
                 this.state = "off";
-                console.log("[NEW STATE]: " + this.state);
+                this.colour = ""
+                console.log(`[NEW STATE]: ${this.holdID} is ${this.state}.`);
                 break
         }
     }  
@@ -45,7 +50,17 @@ for (let i = 0; i < buttons.length; i++) {
 
 buttons.map( button=> {
     button.addEventListener('click', (e) => {
-        let hold = Holds[e.target.innerHTML]
-        hold.changeState()
+        let hold = Holds[e.target.innerHTML];
+        hold.changeState();
+        button.style.backgroundColor = hold.colour;
+        button.style.opacity = 0.5;
+    })
+})
+
+let resetButton = Array.from(document.getElementsByClassName('rest-button'));
+
+resetButton.map ( button=> {
+    button.addEventListener('click', (e) => {
+        
     })
 })
