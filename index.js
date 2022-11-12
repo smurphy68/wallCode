4df6cdbb09b2df5b3ba6b9950eda8b5baaf5afb5
smurphy68 <smurphy68@live.co.uk>
2022-11-12 20:04:25 +0000

    reshuffle

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+app.use(express.static('public'))
