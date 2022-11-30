7ea6fb9d1581b07fcea9e6da941f77ac852f9693
maddocash <31258232+maddocash@users.noreply.github.com>
2022-11-30 15:30:01 +0000

    working routes

--- index.js
+++ index.js
@@ -20,15 +20,29 @@ app.use(express.json({
 // handles post request from the setting page. Getting it into the data base works
 app.post("/db", (request, response) => {
     //console.log(request.body)
-    route = request.body.holds.Holds
-    details = request.body.details
+    // In JS you should always declare let or const for variables
+    // the reason this worked before was because functions are really objects under the hood but shhhh
+    const route = request.body.holds.Holds
+    const details = request.body.details
+    // above could be written like 
+    // const {
+    //   holds: { Holds: route },
+    //   details,
+    // } = request.body;
 
     database.insert({
-        route: route,
-        routename: details.routename,
-        setter: details.setter,
-        grade: details.grade,
-        attempts: details.attempts
+      route: route,
+      routename: details.routename,
+      setter: details.setter,
+      grade: details.grade,
+      attempts: details.attempts
+    },
+    (error, newData) => {
+        if (error) {
+            return response.status(500).json(error)
+        }
+
+        return response.send('route saved')
     });
 })
 
@@ -40,7 +54,7 @@ app.post("/sendRoute", (request, response) => {
         // -- this sucessfully retrieves a JSON object from the database
             // -- Ive tried both
         //let objecttoSend = data[0]
-        let objecttoSend = JSON.stringify(data[0])
+        // let objecttoSend = JSON.stringify(data[0])
         
         // -- this sends a response but the browser thinks the response is empty... 
             //  == Ive tried them all
@@ -49,5 +63,36 @@ app.post("/sendRoute", (request, response) => {
         //response.json("Please...")
         //response.json(JSON.stringify(objecttoSend))
         //response.send(JSON.stringify(objecttoSend))
+
+        if (error) {
+            // 500 = generic server error
+            return response.status(500).send(error)
+        }
+        // if you want all the data
+        return response.json(data[0])
+    })
+})
+
+// GET for asking for data from the server
+// POST is for saving new data
+
+// I'd suggest something like this 
+// GET http://localhost:5000/routes?search=INSERT_ROUTE_NAME
+// To get one route
+// GET http://localhost:5000/routes
+// To get all routes in db
+app.get('/routes', (request, response) => {
+    const searchTerm = request.query.search
+
+    if (searchTerm) {
+        dbQuery.routename = searchTerm
+    }
+    // L87 - L90 can be written like const dbQuery = {...searchTerm && {routename: searchTerm}} if you wanna be fancy
+    database.find(dbQuery, (error, data) => {
+        if (error) {
+            return response.status(500).send(error)
+        }
+
+        return response.json({ routes: data })
     })
 })
